c9d0c5ee2b7dce542c4a814a85700abfb6277772
Rafael Da Cunha Garcia <39766039+Rafiibro@users.noreply.github.com>
2019-03-13 10:21:21 +0100

    FileNumberingFilterWriter finished

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -19,23 +19,55 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
+  private int tab = 0;
+  private int test = 0;
+
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if (test == 0) {
+      out.write("" + ++tab + "\t");
+      test = 1;
+    }
+    for (int i = off; i < (off + len); ++i) {
+      out.write(str.charAt(i));
+      if ( (i+1 < off+len) && str.charAt(i) == '\r' && str.charAt(i+1) == '\n') {
+
+      } else if (str.charAt(i) == '\n' || str.charAt(i) == '\r' ) {
+        out.write("" + ++tab + "\t");
+      }
+    }
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if (test == 0) {
+      out.write("" + ++tab + "\t");
+      test = 1;
+    }
+    for (int i = off; i < (off + len); ++i) {
+      out.write(cbuf[i]);
+      if ( (i+1 < off+len) && cbuf[i] == '\r' && cbuf[i] == '\n') {
+
+      } else if (cbuf[i] == '\n' || cbuf[i] == '\r' ) {
+        out.write("" + ++tab + "\t");
+      }
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if (test == 0) {
+      out.write("" + ++tab + "\t");
+      test = 1;
+    }
+    out.write((char)c);
+    if ((char)c == '\n') {
+      out.write("" + ++tab + "\t");
+    }
   }
 
 }
